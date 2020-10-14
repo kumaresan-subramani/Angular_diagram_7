132e94275e222916f62e08f5c8fbe2df3e2ecfa2
Saranya Murugasamy <saranya.murugasamy@syncfusion.com>
2020-10-14 09:08:14 +0530

    config(EJ2-000): samples added

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DiagramModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
